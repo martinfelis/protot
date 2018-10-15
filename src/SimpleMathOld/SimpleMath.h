f820897417d4ec9f2cf9e8d5d825b1863ff35222
Martin Felis <martin@fysx.org>
2018-10-15 20:42:27 +0200

    Made it simpler to switch between old and new SimpleMath

---
+++ src/SimpleMathOld/SimpleMath.h
@@ -0,0 +1,11 @@
+#ifndef _SIMPLEMATH_H
+#define _SIMPLEMATH_H
+
+#include "SimpleMathFixed.h"
+#include "SimpleMathDynamic.h"
+#include "SimpleMathMixed.h"
+#include "SimpleMathQR.h"
+#include "SimpleMathCommaInitializer.h"
+#include "SimpleMathGL.h"
+
+#endif /* _SIMPLEMATH_H */
--- src/math_types.h
+++ src/math_types.h
@@ -1,6 +1,8 @@
 #ifndef MESHUP_CONFIG_H
 #define MESHUP_CONFIG_H
 
+#ifdef NEW_SIMPLE_MATH
+
 #include "SimpleMath/SimpleMath.h"
 
 typedef SimpleMath::Matrix<float, 2, 1> Vector2f;
@@ -22,5 +24,32 @@ typedef SimpleMath::Matrix<double, 3, 3> Matrix33d;
 
 typedef SimpleMath::Matrix<double> VectorNd;
 typedef SimpleMath::Matrix<double> MatrixNNd;
+
+#else
+
+#include "SimpleMathOld/SimpleMath.h"
+
+typedef SimpleMath::Fixed::Matrix<float, 2, 1> Vector2f;
+typedef SimpleMath::Fixed::Matrix<float, 2, 2> Matrix22f;
+
+typedef SimpleMath::Fixed::Matrix<float, 3, 1> Vector3f;
+typedef SimpleMath::Fixed::Matrix<float, 3, 3> Matrix33f;
+
+typedef SimpleMath::Fixed::Matrix<float, 4, 1> Vector4f;
+typedef SimpleMath::Fixed::Matrix<float, 4, 4> Matrix44f;
+
+typedef SimpleMath::GL::Quaternion Quaternion;
+
+typedef SimpleMath::Dynamic::Matrix<float> VectorNf;
+typedef SimpleMath::Dynamic::Matrix<float> MatrixNNf;
+
+typedef SimpleMath::Fixed::Matrix<double, 3, 1> Vector3d;
+typedef SimpleMath::Fixed::Matrix<double, 3, 3> Matrix33d;
+
+typedef SimpleMath::Dynamic::Matrix<double> VectorNd;
+typedef SimpleMath::Dynamic::Matrix<double> MatrixNNd;
+
+#endif
+
 
 #endif
